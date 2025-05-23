129fb846126186c2ee6ed4e6011590cc83bf9c0f
Aristella Aurum <saltfishe7@gmail.com>
2025-05-23 22:26:34 +0700

    better buttons on archive
    N/A

--- resources/views/reports/partials/actions.blade.php
+++ resources/views/reports/partials/actions.blade.php
@@ -1,10 +1,10 @@
-    
-    
+<div class="d-flex justify-content-center align-items-center">
     <!-- Edit Button -->
-    <button class="btn btn-sm btn-primary btn-edit" data-id="{{ $row->report_id  }}" data-toggle="modal" data-target="#editReportModal">
+    <button class="btn btn-sm btn-primary btn-edit mr-2" data-id="{{ $row->report_id  }}" data-toggle="modal" data-target="#editReportModal">
         <i class="fas fa-edit"></i>
     </button>
     <!-- Delete Button -->
-    <button class="btn btn-sm btn-danger btn-delete" data-id="{{ $row->report_id  }}" data-toggle="modal" data-target="#deleteReportModal">
+    <button class="btn btn-sm btn-danger btn-delete mr-2" data-id="{{ $row->report_id  }}" data-toggle="modal" data-target="#deleteReportModal">
         <i class="fas fa-trash-alt"></i>
     </button>
+</div>
